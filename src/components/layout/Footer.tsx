6e5d096289aef71be4e567d4b8df39d50d2160aa
cookiesheep <wuqf9@mail2.sysu.edu.cn>
2025-04-16 09:39:44 +0800

    添加工信部备案

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -39,7 +39,7 @@ export function Footer() {
               <div className='flex flex-col justify-center items-start'>
                 <div className='flex flex-row justify-end items-center gap-2'>
                   <p className="text-sm text-muted-foreground">
-                    &copy; {new Date().getFullYear()} {name}. All rights reserved.
+                    &copy; {new Date().getFullYear()} {name}. 2025382514
                   </p>
                   <ThemeToggle />
                 </div>
